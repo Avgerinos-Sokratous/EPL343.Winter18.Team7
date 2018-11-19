2b7b1c4b0ae52ec52be7646d6cc1484e65ac5f69
Andreas <42941343+akikas01@users.noreply.github.com>
2018-11-19 19:13:20 +0200

    Add files via upload

--- Login SignUp Form/functions_signup.js
+++ Login SignUp Form/functions_signup.js
@@ -9,5 +9,37 @@ function checkPassword(form) {
 	}
 	else {
 		return true;
-	}
+	}}
+/**
+ * @author akikas01
+ */
+function authorizeForm() {
+	var Username =document.forms["signupform"]["username"].value;
+    var Name = document.forms["signupform"]["fname"].value;
+    var Surname = document.forms["signupform"]["lname"].value;
+    var Email = document.forms["signupform"]["email"].value;
+    var Telephone =  document.forms["signupform"]["telephone"].value;
+   var Password = document.forms["signupform"]["psw"].value;
+    var CPassword = document.forms["signupform"]["password_confirm"].value;
+   var success=new Boolean(1);
+   if(!findname(Username)){success= Booelan(0);}
+  
+ localStorage.setItem(Username, Password);
+  localStorage.setItem("Email"+Username, Email);
+  localStorage.setItem("Name"+Username, Name);
+  localStorage.setItem("Surname"+Username, Surname);
+  localStorage.setItem("Telephone"+Username, Telephone);
+  if(success){alert("You have succesfully registered!!!");}
+    
+}
+/**
+ * @author akikas01
+ * @param {Object} un username
+ */
+function findname(un){
+	if(localStorage.getItem(un)!= null){
+alert("Username already in use");
+return false;
+}
+return true;
 }
