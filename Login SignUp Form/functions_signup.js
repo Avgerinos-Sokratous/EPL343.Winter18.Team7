9c6c31020cf96297809ffd1628e51f77d2887e48
mandre29 <42940819+mandre29@users.noreply.github.com>
2018-11-19 19:08:46 +0200

    Update functions_signup.js

--- Login SignUp Form/functions_signup.js
+++ Login SignUp Form/functions_signup.js
@@ -2,13 +2,12 @@
  * @author mandre29
  */
 
- 
 function checkPassword(form) {
-	if (form.password.value != form.password_confirm.value)
-	{
-		alert('Password is not the same as above!');
+	if (form.password.value != form.password_confirm.value){
+		alert('Password is not the same as password confirm!');
 		return false;
-	} else {
+	}
+	else {
 		return true;
 	}
 }
