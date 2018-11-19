902e1ddbf60234eb8ec8db2961702e5610e6bbd9
Andreas <42941343+akikas01@users.noreply.github.com>
2018-11-19 19:58:47 +0200

    Add files via upload

--- Login SignUp Form/functions_signup.js
+++ Login SignUp Form/functions_signup.js
@@ -13,17 +13,17 @@ function checkPassword(form) {
 /**
  * @author akikas01
  */
-function authorizeForm() {
+function authorizeForm(form0) {
 	var Username =document.forms["signupform"]["username"].value;
     var Name = document.forms["signupform"]["fname"].value;
     var Surname = document.forms["signupform"]["lname"].value;
     var Email = document.forms["signupform"]["email"].value;
     var Telephone =  document.forms["signupform"]["telephone"].value;
-   var Password = document.forms["signupform"]["psw"].value;
+   var Password = document.forms["signupform"]["password"].value;
     var CPassword = document.forms["signupform"]["password_confirm"].value;
    var success=new Boolean(1);
-   if(!findname(Username)){success= Booelan(0);}
-  
+   if(!findname(Username)){success= Boolean(0);}
+checkPassword(form0);
  localStorage.setItem(Username, Password);
   localStorage.setItem("Email"+Username, Email);
   localStorage.setItem("Name"+Username, Name);
