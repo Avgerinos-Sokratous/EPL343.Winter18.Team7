45a07455d4e6e3215f647d6fb47c99c18697a9f8
Andreas <42941343+akikas01@users.noreply.github.com>
2018-11-21 10:52:20 +0200

    Add files via upload

---
+++ Login SignUp Form/Final/check_login_input.js
@@ -0,0 +1,48 @@
+/**
+ * @author mandre29
+ */
+function goToPage(form){ 
+        var Username =form.username.value;
+        var Passsword =form.password.value;
+        var type=localStorage.getItem("Type"+Username);
+       
+        if(type==null){
+  			window.open("Customer.html");
+    	}
+    	else{
+    		if(type=='E'){
+  			   window.open("Employee.html");
+  			}
+   			else{
+ 			   window.open("Manager.html");
+        	}
+   		}
+}
+
+function checkInput(form){
+	
+  	
+	var username = form.username.value;
+	var password= form.password.value;
+
+	if(!checkNamePassword(username, password)){	
+		goToPage(form);
+	}
+}
+
+function checkNamePassword(username, password){
+	if(localStorage.getItem(username)== null){
+			alert('Username does not exist!');
+			return false;
+	}
+	for (var i = 0; i < localStorage.length; i++) {
+    	var key = localStorage.key(i);
+	    var value = localStorage.getItem(key);
+
+		if(key == username && value != password){
+			alert('Password does not match with this username!');
+			return false;
+		}
+	}
+		
+}
