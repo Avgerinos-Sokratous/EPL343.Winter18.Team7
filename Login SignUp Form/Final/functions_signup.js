495c22487da378be5e9dc9293c67b28bd5f85b8c
mandre29 <42940819+mandre29@users.noreply.github.com>
2018-11-23 11:59:20 +0200

    Update functions_signup.js

--- Login SignUp Form/Final/functions_signup.js
+++ Login SignUp Form/Final/functions_signup.js
@@ -9,30 +9,30 @@ function checkPassword(form) {
 	}
 	else {
 		return true;
-	}}
+	}
+}
 /**
  * @author akikas01
  */
 function authorizeForm(form0) {
 	var Username =document.forms["signupform"]["username"].value;
-    var Name = document.forms["signupform"]["fname"].value;
-    var Surname = document.forms["signupform"]["lname"].value;
-    var Email = document.forms["signupform"]["email"].value;
-    var Telephone =  document.forms["signupform"]["telephone"].value;
-   var Password = document.forms["signupform"]["password"].value;
-    var CPassword = document.forms["signupform"]["password_confirm"].value;
-   var success=new Boolean(1);
-   if(!findname(Username)){success= Boolean(0);}
-if(!checkPassword(form0)){
-	
+    	var Name = document.forms["signupform"]["fname"].value;
+	var Surname = document.forms["signupform"]["lname"].value;
+	var Email = document.forms["signupform"]["email"].value;
+	var Telephone =  document.forms["signupform"]["telephone"].value;
+	var Password = document.forms["signupform"]["password"].value;
+	var CPassword = document.forms["signupform"]["password_confirm"].value;
+	var success=new Boolean(1);
+	if(!findname(Username)){success= Boolean(0);}
+	if(!checkPassword(form0)){
 	success= Boolean(0);
-}
- localStorage.setItem(Username, Password);
-  localStorage.setItem("Email"+Username, Email);
-  localStorage.setItem("Name"+Username, Name);
-  localStorage.setItem("Surname"+Username, Surname);
-  localStorage.setItem("Telephone"+Username, Telephone);
-  if(success){alert("You have succesfully registered!!!");}
+	}
+ 	localStorage.setItem(Username, Password);
+	localStorage.setItem("Email"+Username, Email);
+	localStorage.setItem("Name"+Username, Name);
+	localStorage.setItem("Surname"+Username, Surname);
+	localStorage.setItem("Telephone"+Username, Telephone);
+	if(success){alert("You have succesfully registered!!!");}
     
 }
 /**
@@ -41,8 +41,8 @@ if(!checkPassword(form0)){
  */
 function findname(un){
 	if(localStorage.getItem(un)!= null){
-alert("Username already in use");
-return false;
-}
-return true;
+		alert("Username already in use");
+		return false;
+	}
+	return true;
 }
