2e2745b0169fbc71fc47f534a85a52038ff166ae
mandre29 <42940819+mandre29@users.noreply.github.com>
2018-11-23 11:56:33 +0200

    Update check_login_input.js

--- Login SignUp Form/Final/check_login_input.js
+++ Login SignUp Form/Final/check_login_input.js
@@ -7,29 +7,29 @@ function goToPage(form){
         var type=localStorage.getItem("Type"+Username);
        
         if(type==null){
-  			window.open("Customer.html");
+  	   window.open("Customer.html");
     	}
     	else{
-    		if(type=='E'){
-  			   window.open("Employee.html");
-  			}
-   			else{
- 			   window.open("Manager.html");
-        	}
-   		}
+    	   if(type=='E'){
+  		window.open("Employee.html");
+  	   }
+   	   else{
+ 		window.open("Manager.html");
+           }
+   	}
 }
 
 function checkInput(form){
-	
   	var success= new Boolean(1);
 	var username = form.username.value;
 	var password= form.password.value;
 
 	if(!checkNamePassword(username, password)){	
 		success= Boolean(0);
-		
 	}
-	if(success){goToPage(form);}
+	if(success){
+		goToPage(form);
+	}
 }
 
 function checkNamePassword(username, password){
@@ -38,13 +38,13 @@ function checkNamePassword(username, password){
 			return false; 
 	}
 	for (var i = 0; i < localStorage.length; i++) {
-    	var key = localStorage.key(i);
-	    var value = localStorage.getItem(key);
+    	   var key = localStorage.key(i);
+	   var value = localStorage.getItem(key);
 
-		if(key == username && value != password){
-			alert('Password does not match with this username!');
-			return false; 
-		}
+	   if(key == username && value != password){
+		alert('Password does not match with this username!');
+		return false; 
+	   }
 	}
 	return true;	
 }
