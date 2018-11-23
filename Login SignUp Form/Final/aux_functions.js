8483c61f7b64411f58bdf6c035cc796d25babb97
mandre29 <42940819+mandre29@users.noreply.github.com>
2018-11-23 11:55:10 +0200

    Update aux_functions.js

--- Login SignUp Form/Final/aux_functions.js
+++ Login SignUp Form/Final/aux_functions.js
@@ -1,29 +1,28 @@
 function changetype() {
-	
 	var Username =document.forms["changeAccount"]["Account's Username"].value;
 	if(localStorage.getItem(Username)== null) {
 		alert("Username does not exist");
-return;
-}
-else{
-	if(localStorage.getItem("Type"+Username)!= null){
-	alert(Username+" already set to type Employee!");
-	return;
-	}else{
-		localStorage.setItem("Type"+Username,'E');	
-	alert(Username+" has succesfully changed to Employee!");
+		return;
 	}
-	return;
-}
+	else{
+		if(localStorage.getItem("Type"+Username)!= null){
+		   alert(Username+" already set to type Employee!");
+		   return;
+		}else{
+		   localStorage.setItem("Type"+Username,'E');	
+		   alert(Username+" has succesfully changed to Employee!");
+		}
+		return;
+	}
   }
+
   function Manager(){ 
-  	
   	localStorage.setItem("Manager", "12345678");
   	localStorage.setItem("EmailManager","manager@cs.ucy.ac.cy");
-  localStorage.setItem("NameManager", "Anonymous");
-  localStorage.setItem("SurnameManager", "Anonymous");
-  localStorage.setItem("TelephoneManager", "12345677890");
-  localStorage.setItem("TypeManager","M");
+ 	localStorage.setItem("NameManager", "Anonymous");
+	localStorage.setItem("SurnameManager", "Anonymous");
+	localStorage.setItem("TelephoneManager", "12345677890");
+	localStorage.setItem("TypeManager","M");
   	alert("Manager account created");
   	return true;
   }
